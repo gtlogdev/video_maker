8c5de3598cc9400caeddd1c45f553558216ff44d
Clayton Veras <clayton.veras@gtt.com.br>
2019-03-27 16:57:33 -0300

    Alterações no modelo inicial para funcionamento na web, utilizando vídeos no formato AVI e alterando o formato de retorno do processamento.

--- movie_api.py
+++ movie_api.py
@@ -1,50 +1,77 @@
-from flask import Flask
+from flask import Flask, Response
 from flask import request, abort
 import video_maker
 import requests
 app = Flask(__name__)
 
+'''
+    Webservice para recebimento de requisição de processamento de vídeo.
+'''
+
 @app.route('/movie_api/process_video/', methods=['POST'])
 def process_video():
     if not request.json or not 'id' in request.json:
         abort(400)
-    
+    print(request.json)
+    try:
+        rotation = int(request.json['rotation'])
+    except:
+        rotation = int("0")
     video_infos = {
         'id': request.json['id'],
         'images_path':request.json['path'],
-        'rotation': request.json['rotation'],
+        'rotation': rotation,
         'images_ext': request.json['files_extension'],
         'video_ext': request.json['video_ext'],
         'return_url': request.json['return_url'],
         'error_url': request.json['error_url']
 
     }
-    create_video(video_infos)
-    return "ok"
-
-
-def create_video(video_infos):
-    video = video_maker.make_video(image_folder=video_infos['images_path'], out_video_name='video',
-                                   out_video_ext=video_infos['video_ext'], rotation_angle=video_infos['rotation'],
-                                   image_ext=video_infos['images_ext'], vid_to_web=True)
-    if video:
-       video_maker.move_images(video_infos['images_path'], video_infos['images_ext'])
-       save_video(video_infos['id'], video_infos['return_url'])
-       print("video created")
+    processed = create_video(video_infos)
+    if processed:
+        return Response("Processed", status=200, mimetype='application/json')
     else:
-        inform_error(video_infos['id'], video_infos['error_url'])
-        print("error")
+        return Response("Fail on processing", status=500, mimetype='application/json')
 
+'''
+    Chamada das funções de criação de vídeos e feedback sobre a geração do vídeo.
+'''
+def create_video(video_infos):
+	try:
+		video = video_maker.make_video(image_folder=video_infos['images_path'], out_video_name='video', out_video_ext=video_infos['video_ext'], rotation_angle=video_infos['rotation'], image_ext=video_infos['images_ext'], vid_to_web=True)
+		if video is not None:
+			video_maker.move_images(video_infos['images_path'], video_infos['images_ext'])
+			save_video(video_infos['id'], video_infos['return_url'])
+			print("video created")
+			return True
+		else:
+			inform_error(video_infos['id'], video_infos['error_url'])
+			print("error")
+			return False
+	except:
+		return False
+
+'''
+    Função de feedback sobre sucesso na geração de vídeo. 
+'''
 def save_video(json_id, return_url):
-    request_url = return_url+json_id
-    r = requests.get(request_url)
-    print(r.status_code)
+    try:
+        request_url = return_url + str(json_id)
+        r = requests.get(request_url)
+        print(r.status_code)
+    except:
+        print("Deu ruim...")
 
-
+'''
+    Função de feedback sobre erro na geração de vídeo. 
+'''
 def inform_error(json_id, error_url):
-    request_url = error_url + json_id
-    r = requests.get(request_url)
-    print(r.status_code)
+    try:
+        request_url = error_url + str(json_id)
+        r = requests.get(request_url)
+        print(r.status_code)
+    except:
+        print("Deu ruim...")
 
 
 if __name__ == '__main__':
--- video_maker.py
+++ video_maker.py
@@ -1,7 +1,20 @@
-from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
+import cv2
+from cv2 import VideoWriter, imread, imwrite
 import os, imutils, shutil
 
-def make_video(image_folder, out_video_name, out_video_ext, rotation_angle, image_ext, vid_to_web, fps=10, size=None, is_color=True, format="FMP4"):
+
+'''
+    Método para juntar as imagens da pasta selecionada em um vídeo no formato AVI.
+    Os parâmetros necessários para o funcionamento são :
+    
+    image_folder - Path da pasta que contem as imagens a serem processadas. Ex.: "/home/logdev/imagens/" [string]
+    out_video_name - Nome do vídeo que será gerado pela função (sem a extensão). Ex.: "video_veiculo" [string]
+    out_video_ext - Extensão do vídeo que será gerado pela função. Ex.: ".mp4" [string]
+    rotation_angle - Angulo de rotação que deverá ser aplicado às imagens. Ex.: -90 [int]
+    image_ext - Extensão das imagens da pasta que serão utilizadas. Ex.: ".jpg" [string]
+    vid_to_web - Flag para chamada da função de transformação do video para formato web. Ex.: True [bool]
+'''
+def make_video(image_folder, out_video_name, out_video_ext, rotation_angle, image_ext, vid_to_web, fps=5, size=None, is_color=True, format="FMP4"):
     """
     Create a video from a list of images.
  
@@ -18,35 +31,48 @@ def make_video(image_folder, out_video_name, out_video_ext, rotation_angle, imag
     It will resize every image to this size before adding them to the video.
     """
     images = [image_folder+img for img in os.listdir(image_folder) if img.endswith(image_ext)]
-    fourcc = VideoWriter_fourcc(*format)
+    fourcc = cv2.cv.CV_FOURCC(*format)
     vid = None
+    image_thumb = None
+    image_thumb_path = None
     images.sort()
     for image in images:
         if not os.path.exists(image):
             raise FileNotFoundError(image)
         print(image)
         img = imread(image)
+        if image_thumb is None and image[-6:] == "00.jpg":
+            image_thumb = imutils.rotate_bound(img, rotation_angle)
+            image_thumb_path = image
         if(rotation_angle):
-            img = imutils.rotate(img, int(rotation_angle))
+            img = imutils.rotate_bound(img, rotation_angle)
         if vid is None:
             if size is None:
                 size = img.shape[1], img.shape[0]
             print(image_folder + out_video_name)
             vid = VideoWriter((image_folder + out_video_name+".avi"), fourcc, float(fps), size, is_color)
-        if size[0] != img.shape[1] and size[1] != img.shape[0]:
-            img = resize(img, size)
         vid.write(img)
-    vid.release()
+    if vid is not None:
+        vid.release()
+    if image_thumb is not None:
+        imwrite(image_thumb_path, image_thumb)
     if vid_to_web:
         make_for_web(image_folder, (out_video_name+".avi"), (out_video_name+out_video_ext))
+    print(vid)
     return vid
 
+'''
+    Função para converter o vídeo original do formato AVI para MP4, utilizando os codecs de vídeo compatíveis com a tag <video> do HTML 5
+'''
 def make_for_web(video_path, original_video_name, video_name):
     original_video= video_path+original_video_name
     new_video= video_path+video_name
-    os.system("ffmpeg -i "+original_video+" -vcodec h264 -acodec aac -strict -2  -q:v 2 "+new_video)
+    os.system("ffmpeg -y -i "+original_video+" -f mp4 -vcodec libx264 -profile:v main -acodec aac " +new_video)
 
-
+'''
+    Função para mover os arquivos de imagem originais para uma nova pasta. A intenção é evitar que o arquivo de vídeo seja alterado após ser 
+    gerado (Chamada dupla [por bug ou erro] do método de geração de vídeo)
+'''
 def move_images(images_folder, image_ext):
     path = images_folder+"thumbs/"
     os.mkdir(path)
